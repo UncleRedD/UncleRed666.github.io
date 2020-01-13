ed1f167ca60e358a5e18edbbfaa321b1c67eba0e
UncleRedD <39307115+UncleRedD@users.noreply.github.com>
2020-01-13 21:50:13 +0800

    new

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+// const myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+// alert("Sexy ooo")
+document.querySelector('img').onclick = function() {
+	let myImg=document.querySelector('img');
+let imgSource=myImg.getAttribute('src')
+if (imgSource==="666.jpg"){
+	myImg.setAttribute('src',"777.jpg")
+}
+else{
+	myImg.setAttribute('src',"666.jpg")
+}
+    // alert('Ouch! Stop poking me!');
+}
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
+
